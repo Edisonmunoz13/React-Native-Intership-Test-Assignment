3397a837a224630aed6b24bfcdb7dbc2359af19d
Edisonmunoz13 <horacioem595>
2022-11-28 09:11:02 -0300

    feature-principal-view

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,29 @@
 import "./form.css";
 
-export default function Form() {
+function Form() {
   return (
-    <section className="container">
-      <div className="register">
-        <div className="col-1">
-          <h2 className="title"> Welcome, Stranger!</h2>
-          <p className="subtitle">
-            Please log in to this form if you wish to pass the exam.
-          </p>
-          <form id="form" className="flex-col">
-            <input className="input" type="text" placeholder="Email" />
-            <input className="input" type="text" placeholder="Password" />
-            <button className="btn"> Login </button>
-          </form>
+    <div>
+      <section className="container">
+        <img className="img" src="Combined Shape-custom.svg" alt="" />
+        <div className="register">
+          <div className="col-1">
+            <h2 className="title"> Welcome, Stranger!</h2>
+            <p className="subtitle">
+              Please log in to this form if you wish to pass the exam.
+            </p>
+            <form id="form" className="flex-col">
+              <input className="input" type="text" placeholder="Email" />
+              <input className="input" type="text" placeholder="Password" />
+              <button className="btn">
+                Login
+                <img className="arrow" src="Union.svg" alt="" />
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 }
+
+export default Form;
